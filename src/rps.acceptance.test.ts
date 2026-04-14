54bda678ca6719cc555363f040d747676b676d5b
Federico Terraneo <f.terraneo@hotmail.com>
2026-04-14 13:50:05 +0200

    draw game

--- src/rps.acceptance.test.ts
+++ src/rps.acceptance.test.ts
@@ -5,12 +5,19 @@ const PAPER = "P"
 const SCISSORS = "S"
 
 const resolveThrow = (opponentThrow: string, playerThrow: string) => {
-    if (
-        opponentThrow === ROCK && playerThrow === PAPER ||
-        opponentThrow === PAPER && playerThrow === SCISSORS ||
-        opponentThrow === SCISSORS && playerThrow === ROCK
-    )
-        return "Player point"
+    switch (opponentThrow) {
+        case ROCK:
+            if (playerThrow === PAPER) return "Player point"
+            if (playerThrow === ROCK) return "Draw"
+            break
+        case PAPER:
+            if (playerThrow === SCISSORS) return "Player point"
+            if (playerThrow === PAPER) return "Draw"
+            break
+        case SCISSORS:
+            if (playerThrow === ROCK) return "Player point"
+            if (playerThrow === SCISSORS) return "Draw"
+    }
 
     return "Opponent point"
 }
@@ -31,6 +38,13 @@ test.each([
     expect(resolveThrow(opponentThrow, playerThrow)).toEqual("Opponent point")
 })
 
+test.each([
+    [PAPER, PAPER],
+    [ROCK, ROCK],
+    [SCISSORS, SCISSORS],
+])("draw", (opponentThrow: string, playerThrow: string) => {
+    expect(resolveThrow(opponentThrow, playerThrow)).toEqual("Draw")
+})
 
 interface QualcosaInbound {
     playGamble(gamble: string[]): string
@@ -50,20 +64,23 @@ class RPS implements QualcosaInbound {
 
     playGamble(gamble: string[]): string {
         let playerPoints = 0
+        let opponentPoints = 0
 
         for (let i = 0; i < 3; i++) {
             const playerGamble = gamble[i]!
             const opponentGamble = this.challengeCatalog.getCurrentChallenge().opponentGamble[i]!
             const result = resolveThrow(opponentGamble, playerGamble)
             playerPoints += result === "Player point" ? 1 : 0
+            opponentPoints += result === "Opponent point" ? 1 : 0
         }
+        if (opponentPoints === playerPoints) return "draw!"
 
         return playerPoints >= 2 ? "player wins!" : "computer wins!"
     }
 
 }
 
-type Challenge = { opponentGamble: string[]; opponent: string; player: string };
+type Challenge = { opponentGamble: string[]; opponent: string; player: string }
 
 class InMemoryChallengeCatalog implements ChallengeCatalog {
     private challenges: Challenge[]
@@ -108,4 +125,20 @@ test("single game, vs computer, computer wins", () => {
     const results = app.playGamble([SCISSORS, SCISSORS, SCISSORS])
     // show results
     expect(results).toEqual("computer wins!")
+})
+
+test("single game, vs computer, draw", () => {
+    // prepare challenge
+    const currentChallenge = {
+        player: "me",
+        opponent: "computer",
+        opponentGamble: [ROCK, PAPER, SCISSORS],
+    }
+    const challengeCatalog = new InMemoryChallengeCatalog([currentChallenge])
+
+    const app = new RPS(challengeCatalog)
+    // submit gamble
+    const results = app.playGamble([ROCK, PAPER, SCISSORS])
+    // show results
+    expect(results).toEqual("draw!")
 })
